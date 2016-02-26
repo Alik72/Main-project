eaeaee1eb44aaccf0252e55c6e749b470ce75c9f
Alik72 <sadvik19@gmail.com>
2016-02-26 15:14:22 +0600

    Создание класса квадрат

---
+++ dds/src/main/java/ru/stga/pft/sandbox/Sguare.java
@@ -0,0 +1,12 @@
+package ru.stga.pft.sandbox;
+
+/**
+ * Created by Homosapiens on 26.02.2016.
+ */
+public class Sguare {
+  public double l;
+
+  public Sguare (double l)  {
+    this.l = l;}
+
+}
