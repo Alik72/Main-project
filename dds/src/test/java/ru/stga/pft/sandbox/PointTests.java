c0f10de96f22a086a3c55d0fcff3cf2a53b9ed84
Alik72 <sadvik19@gmail.com>
2016-02-29 14:44:58 +0600

    создание своего групп тест

---
+++ dds/src/test/java/ru/stga/pft/sandbox/PointTests.java
@@ -0,0 +1,16 @@
+package ru.stga.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Homosapiens on 29.02.2016.
+ */
+@Test
+public class PointTests {
+  public void testDistance()
+  {
+    Point p = new Point(1,2,4,6);
+    Assert.assertEquals(p.distance(),5.0);
+  }
+}
