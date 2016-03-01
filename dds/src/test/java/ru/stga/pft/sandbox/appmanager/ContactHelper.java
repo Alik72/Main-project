ecf9adb79b68849c43d5cebc9f7eb61cab6b563b
Alik72 <sadvik19@gmail.com>
2016-03-01 11:57:34 +0600

    Создал общий базовый класс BaseHelper - перенес в него метод type, click

--- dds/src/test/java/ru/stga/pft/sandbox/appmanager/ContactHelper.java
+++ dds/src/test/java/ru/stga/pft/sandbox/appmanager/ContactHelper.java
@@ -8,40 +8,34 @@ import ru.stga.pft.sandbox.model.GroupData;
 /**
  * Created by Homosapiens on 01.03.2016.
  */
-public class ContactHelper {
+public class ContactHelper extends HelperBase {
 
-  private FirefoxDriver wd;
 
   public ContactHelper(FirefoxDriver wd) {
-    this.wd = wd;
+
+    super(wd);
   }
 
 
   public void initContactCreation() {
-    wd.findElement(By.linkText("add new")).click();
+
+    click(By.linkText("add new"));
   }
 
 
   public void submitContactCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    wd.findElement(By.name("address2")).click();
-    wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys(contactData.getAddress());
+    type(By.name("firstname"), contactData.getFirstname());
+    type(By.name("lastname"), contactData.getLastname());
+    type(By.name("home"), contactData.getHomePhone());
+    type(By.name("email"), contactData.getEmail());
+    type(By.name("address2"), contactData.getAddress());
+
+
   }
 
 
