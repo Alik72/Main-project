5bbf10b8fe2e127ba22e52b9414ef962f106df08
Alik72 <sadvik19@gmail.com>
2016-03-04 18:09:57 +0600

    Создание теста Eguation

---
+++ dds/src/test/java/ru/stga/pft/sandbox/tests/EguationTests.java
@@ -0,0 +1,42 @@
+package ru.stga.pft.sandbox.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stga.pft.sandbox.Eguation;
+
+
+
+public class EguationTests {
+  @Test
+  public void test0() {
+    Eguation e = new Eguation(1, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+  @Test
+  public void test1() {
+    Eguation e = new Eguation(1, 2, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+  @Test
+  public void test2() {
+    Eguation e = new Eguation(1, 5, 6);
+    Assert.assertEquals(e.rootNumber(), 2);
+  }
+  @Test
+  public void testLinear() {
+    Eguation e = new Eguation(0, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+  @Test
+  public void testConstant() {
+    Eguation e = new Eguation(0, 0, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+  @Test
+  public void testZero() {
+    Eguation e = new Eguation(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(), -1);
+  }
+}
+
+
