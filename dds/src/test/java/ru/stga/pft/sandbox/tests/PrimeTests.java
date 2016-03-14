181a2918ba7aeabe9d5f7b2e6ef3869775a9f547
Alik72 <sadvik19@gmail.com>
2016-03-14 18:24:55 +0600

    Создание класса и теста Примарис

---
+++ dds/src/test/java/ru/stga/pft/sandbox/tests/PrimeTests.java
@@ -0,0 +1,25 @@
+package ru.stga.pft.sandbox.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stga.pft.sandbox.Primes;
+
+/**
+ * Created by Homosapiens on 14.03.2016.
+ */
+public class PrimeTests {
+  @Test
+  public void testPrime()  {
+    Assert.assertTrue(Primes.isPrimesFast(Integer.MAX_VALUE));
+  }
+  @Test (enabled = false)
+  public void testPrimeLong()  {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrimes(n));
+  }
+
+  @Test
+  public void testNonPrime()  {
+    Assert.assertFalse(Primes.isPrimes(Integer.MAX_VALUE - 2));
+  }
+}
