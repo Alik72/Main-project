7fa6dac58bbcd02594ecc0da8807463766e34b6b
Alik72 <sadvik19@gmail.com>
2016-03-15 14:37:38 +0600

    Создание коллекции пример 5

--- dds/src/main/java/ru/stga/pft/sandbox/Collections.java
+++ dds/src/main/java/ru/stga/pft/sandbox/Collections.java
@@ -2,6 +2,7 @@ package ru.stga.pft.sandbox;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -11,10 +12,8 @@ import java.util.List;
 public class Collections {
   public static void main(String[] args) {
     String[] langs = {"JAVA", "C#", "Python", "PHP"};
-    List<String> languages = new ArrayList<String>();
-    languages.add("JAVA");
-    languages.add("C#");
-    languages.add("Python");
+
+    List<String> languages = Arrays.asList("JAVA", "C#");
 
     for (String  l : languages) {
       System.out.println("Я хочу выучить язык программирования " + l);
