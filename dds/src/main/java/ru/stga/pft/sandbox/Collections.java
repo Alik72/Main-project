76c1e07f13275fffe122da98c85c0e3785dba2d0
Alik72 <sadvik19@gmail.com>
2016-03-15 13:47:46 +0600

    Создание коллекции пример 2

--- dds/src/main/java/ru/stga/pft/sandbox/Collections.java
+++ dds/src/main/java/ru/stga/pft/sandbox/Collections.java
@@ -5,13 +5,10 @@ package ru.stga.pft.sandbox;
  */
 public class Collections {
   public static void main(String[] args) {
-    String[] langs = new String[4];
-    langs[0] = "JAVA";
-    langs[1] = "C#";
-    langs[2] = "Python";
-    langs[3] = "PHP";
+    String[] langs = {"JAVA", "C#", "Python", "PHP"};
+
     for (int i = 0; i < langs.length; i++) {
-      System.out.println("Я хочу знать язык " + langs[i]);
+      System.out.println("Я хочу выучить язык программирования " + langs[i]);
     }
   }
 }
