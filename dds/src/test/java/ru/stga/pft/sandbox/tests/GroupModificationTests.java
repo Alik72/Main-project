62295be7550c2e83456ff71e8253a1ff2bdbdb28
Alik72 <sadvik19@gmail.com>
2016-03-17 18:06:53 +0600

    Revert "Revert "Добавление идентификатора id в групп дата""
    This reverts commit 3e0ebd6c22e8a1418969f62fd94ed34f2bc323da.

--- dds/src/test/java/ru/stga/pft/sandbox/tests/GroupModificationTests.java
+++ dds/src/test/java/ru/stga/pft/sandbox/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stga.pft.sandbox.model.GroupData;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 /**
@@ -19,10 +21,16 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1 );
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("proba11", "proba22", "proba33"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+
   }
 }
