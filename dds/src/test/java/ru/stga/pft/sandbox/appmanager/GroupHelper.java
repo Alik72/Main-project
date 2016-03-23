0649955f40ac6de5e8457f8df8adb7e2544b2ae1
Alik72 <sadvik19@gmail.com>
2016-03-23 15:45:23 +0600

    создание методов по удалению и модификации

--- dds/src/test/java/ru/stga/pft/sandbox/appmanager/GroupHelper.java
+++ dds/src/test/java/ru/stga/pft/sandbox/appmanager/GroupHelper.java
@@ -61,18 +61,31 @@ public class GroupHelper extends HelperBase {
     click(By.name("update"));
   }
 
-  public void createGroup(GroupData group) {
+  public void create(GroupData group) {
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
     returnToGroupPage();
   }
+  public void modify(int index, GroupData group) {
+    selectGroup(index);
+    initGroupModification();
+    fillGroupForm(group);
+    submitGroupModification();
+    returnToGroupPage();}
+
+  public void delete(int index) {
+    selectGroup(index);
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
 
   public boolean isThereAGroup() {
     return isElementPresent(By.name("selected[]"));
   }
 
-  public List<GroupData> getGroupList() {
+  public List<GroupData> list() {
     List<GroupData> groups = new ArrayList<GroupData>();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
