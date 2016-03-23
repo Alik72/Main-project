09cbaadb24e1f865bf131e142a2a362b474aee63
Alik72 <sadvik19@gmail.com>
2016-03-23 15:47:00 +0600

    Изменяем условие проверки у метода предусловий по проверки присутствия групп

--- dds/src/test/java/ru/stga/pft/sandbox/tests/GroupDeletionTests.java
+++ dds/src/test/java/ru/stga/pft/sandbox/tests/GroupDeletionTests.java
@@ -2,42 +2,35 @@ package ru.stga.pft.sandbox.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
+
 import ru.stga.pft.sandbox.model.GroupData;
 
-import static org.openqa.selenium.OutputType.*;
-
 public class GroupDeletionTests extends TestBase {
-
+    @BeforeMethod
+    public void ensurePrecondition () {
+        app.goTo().groupPage();
+        if (app.group().list().size() == 0 ) {
+            app.group().create(new GroupData("test1", null, null));
+        }
+    }
     @Test
     public void testGroupDeletion() {
-        app.getNavigationHelper().gotoGroupPage();
-        if(! app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-        }
-        List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 1 );
-        app.getGroupHelper().deleteSelectedGroups();
-        app.getGroupHelper().returnToGroupPage();
-        List<GroupData> after = app.getGroupHelper().getGroupList();
+        List<GroupData> before = app.group().list();
+        int index = before.size() - 1;
+        app.group().delete(index);
+        List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(),before.size() - 1 );
 
-        before.remove(before.size() - 1 );
+        before.remove(index);
         Assert.assertEquals(before, after);
         }
 
 
-    }
+
+
+}
 
 
