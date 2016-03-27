82629d4273cc0ad15f012f636bff8f0755b6da02
Alik72 <sadvik19@gmail.com>
2016-03-27 17:53:03 +0600

    УРОК 4 упражнение с 5 по 10 добавление

--- dds/src/test/java/ru/stga/pft/sandbox/model/ContactData.java
+++ dds/src/test/java/ru/stga/pft/sandbox/model/ContactData.java
@@ -1,20 +1,36 @@
 package ru.stga.pft.sandbox.model;
 
 public class ContactData {
+  private int id;
   private final String lastname;
   private final String firstname;
-   private final String homePhone;
+  private final String homePhone;
   private final String email;
   private final String address;
   private String group;
 
-  public ContactData(String lastname,String firstname, String homePhone, String email, String address, String group) {
+  public ContactData(String lastname, String firstname, String homePhone, String email, String address, String group) {
+    this.id = Integer.MAX_VALUE;
     this.lastname = lastname;
     this.firstname = firstname;
     this.homePhone = homePhone;
     this.email = email;
     this.address = address;
     this.group = group;
+  }
+
+  public ContactData(int id,String lastname, String firstname, String homePhone, String email, String address, String group) {
+    this.id = id;
+    this.lastname = lastname;
+    this.firstname = firstname;
+    this.homePhone = homePhone;
+    this.email = email;
+    this.address = address;
+    this.group = group;
+  }
+
+  public int getId() {
+    return id;
   }
 
   public String getFirstname() {
@@ -40,4 +56,38 @@ public class ContactData {
   public String getGroup() {
     return group;
   }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+    return firstname != null ? firstname.equals(that.firstname) : that.firstname == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = lastname != null ? lastname.hashCode() : 0;
+    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "id='" + id + '\'' +
+            ", lastname='" + lastname + '\'' +
+            ", firstname='" + firstname + '\'' +
+            '}';
+  }
 }
+
+
