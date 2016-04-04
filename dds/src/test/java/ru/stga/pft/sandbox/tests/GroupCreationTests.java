48df0871a163c52011ed4271c0d9513f3a72591b
Alik72 <sadvik19@gmail.com>
2016-04-04 14:45:13 +0600

    Урок 6 занятие 4. Создали цикл по созданию групп - 3 раза.

--- dds/src/test/java/ru/stga/pft/sandbox/tests/GroupCreationTests.java
+++ dds/src/test/java/ru/stga/pft/sandbox/tests/GroupCreationTests.java
@@ -16,29 +16,27 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
-
-
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2");
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo (before.size() + 1));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    String[] names = new String[]{"test1", "test2", "test3"};
+    for (String name : names) {
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      GroupData group = new GroupData().withName(name);
+      app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size() + 1));
+      Groups after = app.group().all();
+      assertThat(after, equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
   }
-
-  @Test
+    @Test (enabled = false)
     public void testBadGroupCreation() {
-
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test'");
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo (before.size()));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(before));
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      GroupData group = new GroupData().withName("test'");
+      app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size()));
+      Groups after = app.group().all();
+      assertThat(after, equalTo(before));
+    }
   }
 
-
-}
