b1df3828d6483666861a9b299bf045cdedf17ad0
Alik72 <sadvik19@gmail.com>
2016-04-06 22:19:57 +0600

    Урок 6 задание 8 использование закрытие файлов при помощи -  try

--- dds/src/test/java/ru/stga/pft/sandbox/tests/GroupCreationTests.java
+++ dds/src/test/java/ru/stga/pft/sandbox/tests/GroupCreationTests.java
@@ -1,7 +1,6 @@
 package ru.stga.pft.sandbox.tests;
 
 
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
@@ -23,57 +22,61 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class GroupCreationTests extends TestBase {
 
-@DataProvider
+  @DataProvider
 
-public Iterator<Object[]> validGroupsFromXml() throws IOException {
-  BufferedReader reader = new BufferedReader (new FileReader(new File("src/test/resources/groups.json")));
-  String json = "";
-  String line = reader.readLine();
-  while (line != null){
-    json+=line;
-    line = reader.readLine();
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+      }.getType());
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
   }
-  Gson gson = new Gson();
-  List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
-  return groups.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
 
-  }
   @DataProvider
 
   public Iterator<Object[]> validGroupsFromJson() throws IOException {
-    BufferedReader reader = new BufferedReader (new FileReader(new File("src/test/resources/groups.xml")));
-    String xml = "";
-    String line = reader.readLine();
-    while (line != null){
-      xml+=line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(GroupData.class);
+      List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(GroupData.class);
-    List<GroupData> groups = (List<GroupData>)xstream.fromXML(xml);
-    return groups.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
+     }
 
-  }
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
-      app.goTo().groupPage();
-      Groups before = app.group().all();
-      app.group().create(group);
-      assertThat(app.group().count(), equalTo(before.size() + 1));
-      Groups after = app.group().all();
-      assertThat(after, equalTo(
-              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    app.goTo().groupPage();
+    Groups before = app.group().all();
+    app.group().create(group);
+    assertThat(app.group().count(), equalTo(before.size() + 1));
+    Groups after = app.group().all();
+    assertThat(after, equalTo(
+            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
   }
-    @Test (enabled = false)
-    public void testBadGroupCreation() {
-      app.goTo().groupPage();
-      Groups before = app.group().all();
-      GroupData group = new GroupData().withName("test'");
-      app.group().create(group);
-      assertThat(app.group().count(), equalTo(before.size()));
-      Groups after = app.group().all();
-      assertThat(after, equalTo(before));
-    }
+
+  @Test(enabled = false)
+  public void testBadGroupCreation() {
+    app.goTo().groupPage();
+    Groups before = app.group().all();
+    GroupData group = new GroupData().withName("test'");
+    app.group().create(group);
+    assertThat(app.group().count(), equalTo(before.size()));
+    Groups after = app.group().all();
+    assertThat(after, equalTo(before));
   }
+}
 
